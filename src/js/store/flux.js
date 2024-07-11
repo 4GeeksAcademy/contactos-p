c3f0e2a99f7d31f615a25b162b700c1aa6e3202f
EduardoHernandezGuzman <edohdezg@gmail.com>
2024-07-11 15:23:51 +0200

    Métodos GET y DELETE funcionando

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,45 +1,58 @@
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
-		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
+  return {
+    store: {
+      contacto: [
+        {
+          name: "",
+          phone: "",
+          email: "",
+          address: "",
+        },
+      ],
+    },
+
+    actions: {
+      traerContactos: async () => {
+        const response = await fetch(
+          "https://playground.4geeks.com/contact/agendas/Edo"
+        );
+        const data = await response.json();
+        setStore({ contacto: data.contacts });
+      },
+
+      eliminarContacto: async (id) => {
+        const response = await fetch(
+          "https://playground.4geeks.com/contact/agendas/Edo/contacts/" + id,
+          { method: "DELETE" }
+        );
+        getActions().traerContactos();
+      },
+
+      // Use getActions to call a function within a fuction
+      exampleFunction: () => {
+        getActions().changeColor(0, "green");
+      },
+      loadSomeData: () => {
+        /**
 					fetch().then().then(data => setStore({ "foo": data.bar }))
 				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+      },
+      changeColor: (index, color) => {
+        //get the store
+        const store = getStore();
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
+        //we have to loop the entire demo array to look for the respective index
+        //and change its color
+        const demo = store.demo.map((elm, i) => {
+          if (i === index) elm.background = color;
+          return elm;
+        });
 
-				//reset the global store
-				setStore({ demo: demo });
-			}
-		}
-	};
+        //reset the global store
+        setStore({ demo: demo });
+      },
+    },
+  };
 };
 
 export default getState;
---
+++ src/js/views/agenda.js
@@ -0,0 +1,31 @@
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
+
+const Agenda = () => {
+  const { store, actions } = useContext(Context);
+  console.log(store.contacto);
+
+  return (
+    <div className="container d-flex justify-content-center">
+      <div className="row">
+        <div className="col">
+          {store.contacto.map((item) => (
+            <div className="card w-200">
+              <div className="card-body">
+                <h5 className="card-title">{item.name}</h5>
+                <p className="card-text">Teléfono: {item.phone}</p>
+                <p className="card-text">Email: {item.email}</p>
+                <p className="card-text">Dirección: {item.address}</p>
+              </div>
+              <button onClick={() => actions.eliminarContacto(item.id)}>
+                Borrar
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Agenda;
